1e7e47b9449c8a80838c9e91882d093a925af100
Yama0219 <cairnhill.sg1109@gmail.com>
2023-02-15 00:52:57 +0900

    remove Analog Watchdog

--- RRER_Battery/cmake-build-debug/CMakeFiles/RRER_Battery.elf.dir/Core/Src/c_values.c.obj.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/RRER_Battery.elf.dir/Core/Src/c_values.c.obj: \
- C:\Users\kenke\Desktop\Univ\T-semi\NHK2023\Ryuzo_github\Tsemi_NHK2023_Firmware\RRER_Battery\Core\Src\c_values.c \
- C:\Users\kenke\Desktop\Univ\T-semi\NHK2023\Ryuzo_github\Tsemi_NHK2023_Firmware\RRER_Battery\Core\Inc/c_values.h
--- RRER_Battery/cmake-build-debug/CMakeFiles/RRER_Battery.elf.dir/Drivers/STM32G4xx_HAL_Driver/Src/stm32g4xx_ll_adc.c.obj.d
+++
@@ -1,2 +0,0 @@
-CMakeFiles/RRER_Battery.elf.dir/Drivers/STM32G4xx_HAL_Driver/Src/stm32g4xx_ll_adc.c.obj: \
- C:\Users\kenke\Desktop\Univ\T-semi\NHK2023\Ryuzo_github\Tsemi_NHK2023_Firmware\RRER_Battery\Drivers\STM32G4xx_HAL_Driver\Src\stm32g4xx_ll_adc.c
