7241ebe3911bd9da84b69ee175a35e504a0e524a
Yama0219 <cairnhill.sg1109@gmail.com>
2023-02-15 00:34:13 +0900

    制御追加

---
+++ RRER_Battery/Core/Inc/c_values.h
@@ -0,0 +1,13 @@
+//
+// Created by kenke on 2022/12/25.
+//
+
+#ifndef POWER_BOARD_C_VALUES_H
+#define POWER_BOARD_C_VALUES_H
+
+#define TP1 4.83f
+#define TP2 0.2025f
+
+float calc_v_ref(int vrefint_value);
+
+#endif //POWER_BOARD_C_VALUES_H
