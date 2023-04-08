381d9f6a0985b2d34b87886895615ab9f0b36893
Yama0219 <cairnhill.sg1109@gmail.com>
2023-04-08 13:19:41 +0900

    add ashimawari

---
+++ 2PID_steer_dataro/cmake-build-debug/CMakeFiles/2PID_steer_dataro.elf.dir/Core/Src/PID.c.obj.d
@@ -0,0 +1,3 @@
+CMakeFiles/2PID_steer_dataro.elf.dir/Core/Src/PID.c.obj: \
+ C:\Users\kenke\Desktop\Univ\T-semi\NHK2023\STM\2PID_steer_dataro\Core\Src\PID.c \
+ C:\Users\kenke\Desktop\Univ\T-semi\NHK2023\STM\2PID_steer_dataro\Core\Inc/PID.h
---
+++ 2PID_steer_dataro/cmake-build-debug/CMakeFiles/2PID_steer_dataro.elf.dir/DependInfo.cmake
@@ -0,0 +1,65 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "ASM"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_ASM
+  "C:/Users/kenke/Desktop/Univ/T-semi/NHK2023/STM/2PID_steer_dataro/Core/Startup/startup_stm32g431kbtx.s" "C:/Users/kenke/Desktop/Univ/T-semi/NHK2023/STM/2PID_steer_dataro/cmake-build-debug/CMakeFiles/2PID_steer_dataro.elf.dir/Core/Startup/startup_stm32g431kbtx.s.obj"
+  )
+set(CMAKE_ASM_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_ASM
+  "DEBUG"
+  "STM32G431xx"
+  "USE_HAL_DRIVER"
+  )
+
+# The include file search paths:
+set(CMAKE_ASM_TARGET_INCLUDE_PATH
+  "C:/Users/kenke/Desktop/Univ/T-semi/NHK2023/STM/2PID_steer_dataro/Core/Inc"
+  "C:/Users/kenke/Desktop/Univ/T-semi/NHK2023/STM/2PID_steer_dataro/Drivers/STM32G4xx_HAL_Driver/Inc"
+  "C:/Users/kenke/Desktop/Univ/T-semi/NHK2023/STM/2PID_steer_dataro/Drivers/STM32G4xx_HAL_Driver/Inc/Legacy"
+  "C:/Users/kenke/Desktop/Univ/T-semi/NHK2023/STM/2PID_steer_dataro/Drivers/CMSIS/Device/ST/STM32G4xx/Include"
+  "C:/Users/kenke/Desktop/Univ/T-semi/NHK2023/STM/2PID_steer_dataro/Drivers/CMSIS/Include"
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "C:/Users/kenke/Desktop/Univ/T-semi/NHK2023/STM/2PID_steer_dataro/Core/Src/PID.c" "CMakeFiles/2PID_steer_dataro.elf.dir/Core/Src/PID.c.obj" "gcc" "CMakeFiles/2PID_steer_dataro.elf.dir/Core/Src/PID.c.obj.d"
+  "C:/Users/kenke/Desktop/Univ/T-semi/NHK2023/STM/2PID_steer_dataro/Core/Src/main.c" "CMakeFiles/2PID_steer_dataro.elf.dir/Core/Src/main.c.obj" "gcc" "CMakeFiles/2PID_steer_dataro.elf.dir/Core/Src/main.c.obj.d"
+  "C:/Users/kenke/Desktop/Univ/T-semi/NHK2023/STM/2PID_steer_dataro/Core/Src/stm32g4xx_hal_msp.c" "CMakeFiles/2PID_steer_dataro.elf.dir/Core/Src/stm32g4xx_hal_msp.c.obj" "gcc" "CMakeFiles/2PID_steer_dataro.elf.dir/Core/Src/stm32g4xx_hal_msp.c.obj.d"
+  "C:/Users/kenke/Desktop/Univ/T-semi/NHK2023/STM/2PID_steer_dataro/Core/Src/stm32g4xx_hal_timebase_tim.c" "CMakeFiles/2PID_steer_dataro.elf.dir/Core/Src/stm32g4xx_hal_timebase_tim.c.obj" "gcc" "CMakeFiles/2PID_steer_dataro.elf.dir/Core/Src/stm32g4xx_hal_timebase_tim.c.obj.d"
+  "C:/Users/kenke/Desktop/Univ/T-semi/NHK2023/STM/2PID_steer_dataro/Core/Src/stm32g4xx_it.c" "CMakeFiles/2PID_steer_dataro.elf.dir/Core/Src/stm32g4xx_it.c.obj" "gcc" "CMakeFiles/2PID_steer_dataro.elf.dir/Core/Src/stm32g4xx_it.c.obj.d"
+  "C:/Users/kenke/Desktop/Univ/T-semi/NHK2023/STM/2PID_steer_dataro/Core/Src/syscalls.c" "CMakeFiles/2PID_steer_dataro.elf.dir/Core/Src/syscalls.c.obj" "gcc" "CMakeFiles/2PID_steer_dataro.elf.dir/Core/Src/syscalls.c.obj.d"
+  "C:/Users/kenke/Desktop/Univ/T-semi/NHK2023/STM/2PID_steer_dataro/Core/Src/sysmem.c" "CMakeFiles/2PID_steer_dataro.elf.dir/Core/Src/sysmem.c.obj" "gcc" "CMakeFiles/2PID_steer_dataro.elf.dir/Core/Src/sysmem.c.obj.d"
+  "C:/Users/kenke/Desktop/Univ/T-semi/NHK2023/STM/2PID_steer_dataro/Core/Src/system_stm32g4xx.c" "CMakeFiles/2PID_steer_dataro.elf.dir/Core/Src/system_stm32g4xx.c.obj" "gcc" "CMakeFiles/2PID_steer_dataro.elf.dir/Core/Src/system_stm32g4xx.c.obj.d"
+  "C:/Users/kenke/Desktop/Univ/T-semi/NHK2023/STM/2PID_steer_dataro/Drivers/STM32G4xx_HAL_Driver/Src/stm32g4xx_hal.c" "CMakeFiles/2PID_steer_dataro.elf.dir/Drivers/STM32G4xx_HAL_Driver/Src/stm32g4xx_hal.c.obj" "gcc" "CMakeFiles/2PID_steer_dataro.elf.dir/Drivers/STM32G4xx_HAL_Driver/Src/stm32g4xx_hal.c.obj.d"
+  "C:/Users/kenke/Desktop/Univ/T-semi/NHK2023/STM/2PID_steer_dataro/Drivers/STM32G4xx_HAL_Driver/Src/stm32g4xx_hal_cortex.c" "CMakeFiles/2PID_steer_dataro.elf.dir/Drivers/STM32G4xx_HAL_Driver/Src/stm32g4xx_hal_cortex.c.obj" "gcc" "CMakeFiles/2PID_steer_dataro.elf.dir/Drivers/STM32G4xx_HAL_Driver/Src/stm32g4xx_hal_cortex.c.obj.d"
+  "C:/Users/kenke/Desktop/Univ/T-semi/NHK2023/STM/2PID_steer_dataro/Drivers/STM32G4xx_HAL_Driver/Src/stm32g4xx_hal_dma.c" "CMakeFiles/2PID_steer_dataro.elf.dir/Drivers/STM32G4xx_HAL_Driver/Src/stm32g4xx_hal_dma.c.obj" "gcc" "CMakeFiles/2PID_steer_dataro.elf.dir/Drivers/STM32G4xx_HAL_Driver/Src/stm32g4xx_hal_dma.c.obj.d"
+  "C:/Users/kenke/Desktop/Univ/T-semi/NHK2023/STM/2PID_steer_dataro/Drivers/STM32G4xx_HAL_Driver/Src/stm32g4xx_hal_dma_ex.c" "CMakeFiles/2PID_steer_dataro.elf.dir/Drivers/STM32G4xx_HAL_Driver/Src/stm32g4xx_hal_dma_ex.c.obj" "gcc" "CMakeFiles/2PID_steer_dataro.elf.dir/Drivers/STM32G4xx_HAL_Driver/Src/stm32g4xx_hal_dma_ex.c.obj.d"
+  "C:/Users/kenke/Desktop/Univ/T-semi/NHK2023/STM/2PID_steer_dataro/Drivers/STM32G4xx_HAL_Driver/Src/stm32g4xx_hal_exti.c" "CMakeFiles/2PID_steer_dataro.elf.dir/Drivers/STM32G4xx_HAL_Driver/Src/stm32g4xx_hal_exti.c.obj" "gcc" "CMakeFiles/2PID_steer_dataro.elf.dir/Drivers/STM32G4xx_HAL_Driver/Src/stm32g4xx_hal_exti.c.obj.d"
+  "C:/Users/kenke/Desktop/Univ/T-semi/NHK2023/STM/2PID_steer_dataro/Drivers/STM32G4xx_HAL_Driver/Src/stm32g4xx_hal_flash.c" "CMakeFiles/2PID_steer_dataro.elf.dir/Drivers/STM32G4xx_HAL_Driver/Src/stm32g4xx_hal_flash.c.obj" "gcc" "CMakeFiles/2PID_steer_dataro.elf.dir/Drivers/STM32G4xx_HAL_Driver/Src/stm32g4xx_hal_flash.c.obj.d"
+  "C:/Users/kenke/Desktop/Univ/T-semi/NHK2023/STM/2PID_steer_dataro/Drivers/STM32G4xx_HAL_Driver/Src/stm32g4xx_hal_flash_ex.c" "CMakeFiles/2PID_steer_dataro.elf.dir/Drivers/STM32G4xx_HAL_Driver/Src/stm32g4xx_hal_flash_ex.c.obj" "gcc" "CMakeFiles/2PID_steer_dataro.elf.dir/Drivers/STM32G4xx_HAL_Driver/Src/stm32g4xx_hal_flash_ex.c.obj.d"
+  "C:/Users/kenke/Desktop/Univ/T-semi/NHK2023/STM/2PID_steer_dataro/Drivers/STM32G4xx_HAL_Driver/Src/stm32g4xx_hal_flash_ramfunc.c" "CMakeFiles/2PID_steer_dataro.elf.dir/Drivers/STM32G4xx_HAL_Driver/Src/stm32g4xx_hal_flash_ramfunc.c.obj" "gcc" "CMakeFiles/2PID_steer_dataro.elf.dir/Drivers/STM32G4xx_HAL_Driver/Src/stm32g4xx_hal_flash_ramfunc.c.obj.d"
+  "C:/Users/kenke/Desktop/Univ/T-semi/NHK2023/STM/2PID_steer_dataro/Drivers/STM32G4xx_HAL_Driver/Src/stm32g4xx_hal_gpio.c" "CMakeFiles/2PID_steer_dataro.elf.dir/Drivers/STM32G4xx_HAL_Driver/Src/stm32g4xx_hal_gpio.c.obj" "gcc" "CMakeFiles/2PID_steer_dataro.elf.dir/Drivers/STM32G4xx_HAL_Driver/Src/stm32g4xx_hal_gpio.c.obj.d"
+  "C:/Users/kenke/Desktop/Univ/T-semi/NHK2023/STM/2PID_steer_dataro/Drivers/STM32G4xx_HAL_Driver/Src/stm32g4xx_hal_pwr.c" "CMakeFiles/2PID_steer_dataro.elf.dir/Drivers/STM32G4xx_HAL_Driver/Src/stm32g4xx_hal_pwr.c.obj" "gcc" "CMakeFiles/2PID_steer_dataro.elf.dir/Drivers/STM32G4xx_HAL_Driver/Src/stm32g4xx_hal_pwr.c.obj.d"
+  "C:/Users/kenke/Desktop/Univ/T-semi/NHK2023/STM/2PID_steer_dataro/Drivers/STM32G4xx_HAL_Driver/Src/stm32g4xx_hal_pwr_ex.c" "CMakeFiles/2PID_steer_dataro.elf.dir/Drivers/STM32G4xx_HAL_Driver/Src/stm32g4xx_hal_pwr_ex.c.obj" "gcc" "CMakeFiles/2PID_steer_dataro.elf.dir/Drivers/STM32G4xx_HAL_Driver/Src/stm32g4xx_hal_pwr_ex.c.obj.d"
+  "C:/Users/kenke/Desktop/Univ/T-semi/NHK2023/STM/2PID_steer_dataro/Drivers/STM32G4xx_HAL_Driver/Src/stm32g4xx_hal_rcc.c" "CMakeFiles/2PID_steer_dataro.elf.dir/Drivers/STM32G4xx_HAL_Driver/Src/stm32g4xx_hal_rcc.c.obj" "gcc" "CMakeFiles/2PID_steer_dataro.elf.dir/Drivers/STM32G4xx_HAL_Driver/Src/stm32g4xx_hal_rcc.c.obj.d"
+  "C:/Users/kenke/Desktop/Univ/T-semi/NHK2023/STM/2PID_steer_dataro/Drivers/STM32G4xx_HAL_Driver/Src/stm32g4xx_hal_rcc_ex.c" "CMakeFiles/2PID_steer_dataro.elf.dir/Drivers/STM32G4xx_HAL_Driver/Src/stm32g4xx_hal_rcc_ex.c.obj" "gcc" "CMakeFiles/2PID_steer_dataro.elf.dir/Drivers/STM32G4xx_HAL_Driver/Src/stm32g4xx_hal_rcc_ex.c.obj.d"
+  "C:/Users/kenke/Desktop/Univ/T-semi/NHK2023/STM/2PID_steer_dataro/Drivers/STM32G4xx_HAL_Driver/Src/stm32g4xx_hal_tim.c" "CMakeFiles/2PID_steer_dataro.elf.dir/Drivers/STM32G4xx_HAL_Driver/Src/stm32g4xx_hal_tim.c.obj" "gcc" "CMakeFiles/2PID_steer_dataro.elf.dir/Drivers/STM32G4xx_HAL_Driver/Src/stm32g4xx_hal_tim.c.obj.d"
+  "C:/Users/kenke/Desktop/Univ/T-semi/NHK2023/STM/2PID_steer_dataro/Drivers/STM32G4xx_HAL_Driver/Src/stm32g4xx_hal_tim_ex.c" "CMakeFiles/2PID_steer_dataro.elf.dir/Drivers/STM32G4xx_HAL_Driver/Src/stm32g4xx_hal_tim_ex.c.obj" "gcc" "CMakeFiles/2PID_steer_dataro.elf.dir/Drivers/STM32G4xx_HAL_Driver/Src/stm32g4xx_hal_tim_ex.c.obj.d"
+  "C:/Users/kenke/Desktop/Univ/T-semi/NHK2023/STM/2PID_steer_dataro/Drivers/STM32G4xx_HAL_Driver/Src/stm32g4xx_hal_uart.c" "CMakeFiles/2PID_steer_dataro.elf.dir/Drivers/STM32G4xx_HAL_Driver/Src/stm32g4xx_hal_uart.c.obj" "gcc" "CMakeFiles/2PID_steer_dataro.elf.dir/Drivers/STM32G4xx_HAL_Driver/Src/stm32g4xx_hal_uart.c.obj.d"
+  "C:/Users/kenke/Desktop/Univ/T-semi/NHK2023/STM/2PID_steer_dataro/Drivers/STM32G4xx_HAL_Driver/Src/stm32g4xx_hal_uart_ex.c" "CMakeFiles/2PID_steer_dataro.elf.dir/Drivers/STM32G4xx_HAL_Driver/Src/stm32g4xx_hal_uart_ex.c.obj" "gcc" "CMakeFiles/2PID_steer_dataro.elf.dir/Drivers/STM32G4xx_HAL_Driver/Src/stm32g4xx_hal_uart_ex.c.obj.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
